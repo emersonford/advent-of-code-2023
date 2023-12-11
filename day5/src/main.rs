ac8b1542b424f4c87f76a347ecd56b8189bb9e35
Emerson Ford <emersontford@gmail.com>
2023-12-10 23:41:16 -0800

    finish day 5 part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -9,7 +9,105 @@ struct MapTuple {
     length: usize,
 }
 
-fn part2() {}
+fn part2() {
+    let mut maps: HashMap<String, (String, Vec<MapTuple>)> = HashMap::new();
+
+    let lines = std::io::stdin()
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<_>>();
+
+    let mut chunks = lines.split(|line| line.is_empty());
+
+    let mut seeds = {
+        let seeds_str = chunks.next().unwrap().first().unwrap();
+
+        let parsed = seeds_str
+            .strip_prefix("seeds: ")
+            .unwrap()
+            .split_whitespace()
+            .map(|seed| seed.parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+
+        parsed
+            .chunks(2)
+            .map(|val| val[0]..(val[0] + val[1]))
+            .collect::<Vec<_>>()
+    };
+
+    seeds.sort_by_key(|val| val.start);
+
+    for chunk in chunks {
+        // Reverse the mapping to search from location -> seed.
+        let (to, from) = chunk
+            .first()
+            .unwrap()
+            .strip_suffix(" map:")
+            .unwrap()
+            .split_once("-to-")
+            .unwrap();
+
+        let mut tuples = chunk
+            .into_iter()
+            .skip(1)
+            .map(|line| {
+                let mut split = line.split(' ').map(|x| x.parse::<usize>().unwrap());
+
+                MapTuple {
+                    fst_start: split.next().unwrap(),
+                    snd_start: split.next().unwrap(),
+                    length: split.next().unwrap(),
+                }
+            })
+            .collect::<Vec<_>>();
+
+        tuples.sort_by_key(|val| val.fst_start);
+
+        maps.insert(from.to_string(), (to.to_string(), tuples));
+    }
+
+    let mut location = 0;
+    let ret = loop {
+        let mut curr = ("location", location);
+
+        while curr.0 != "seed" {
+            let entry = maps.get(curr.0).unwrap();
+
+            let new_val = if let Ok(idx) = entry.1.binary_search_by(|val| {
+                if val.fst_start <= curr.1 && curr.1 < val.fst_start + val.length {
+                    return Ordering::Equal;
+                }
+
+                val.fst_start.cmp(&curr.1)
+            }) {
+                let tuple_entry = entry.1.get(idx).unwrap();
+
+                tuple_entry.snd_start + (curr.1 - tuple_entry.fst_start)
+            } else {
+                curr.1
+            };
+
+            curr = (&entry.0, new_val);
+        }
+
+        if seeds
+            .binary_search_by(|val| {
+                if val.contains(&curr.1) {
+                    return Ordering::Equal;
+                }
+
+                val.start.cmp(&curr.1)
+            })
+            .is_ok()
+        {
+            break location;
+        }
+
+        location += 1;
+    };
+
+    println!("{ret}");
+}
 
 fn part1() {
     let mut maps: HashMap<String, (String, Vec<MapTuple>)> = HashMap::new();
